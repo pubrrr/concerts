e1e5f0a9c0981076532d3d121205e98fc015be2c
pubrrr <pubr@posteo.de>
2024-12-27 18:38:08 +0100

    feat: add button to scroll to top on mobile

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -3,9 +3,10 @@ import { StyledLink } from './StyledLink.tsx';
 
 function App() {
     return (
-        <div className='flex flex-col items-center text-center text-base-content'>
+        <div className='relative flex flex-col items-center text-center text-base-content'>
+            <ScrollToTop />
             <div className='m-8 flex flex-col items-center'>
-                <h1 className='mb-4 text-3xl font-bold'>All Concerts in Munich</h1>
+                <h1 className='mb-4 text-3xl font-bold text-primary'>All Concerts in Munich</h1>
 
                 <p>
                     <a className='link' href='https://t.me/MunichMetalConcerts'>
@@ -24,7 +25,7 @@ function App() {
                 </p>
             </div>
             <ConcertList />
-            <p className='text-xs'>
+            <p className='mb-16 text-xs'>
                 Emoji artwork is provided by <StyledLink href='https://emojitwo.github.io/'>Emojitwo</StyledLink>,
                 originally released as <StyledLink href='https://www.emojione.com/'>Emojione 2.2</StyledLink> by{' '}
                 <StyledLink href='http://www.ranks.com/'>Ranks.com</StyledLink> with contributions from the Emojitwo
@@ -37,5 +38,18 @@ function App() {
         </div>
     );
 }
+
+const ScrollToTop = () => (
+    <div className='absolute bottom-0 right-2 top-0 flex h-full items-end pt-[110vh] sm:hidden'>
+        <a
+            role='button'
+            href='#'
+            className='btn btn-circle btn-accent sticky bottom-4 z-[999]'
+            aria-label='Scroll to top'
+        >
+            <span className='iconify mdi--chevron-up text-2xl' />
+        </a>
+    </div>
+);
 
 export default App;
