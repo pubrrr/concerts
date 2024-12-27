4022cec05d551c4c975bc1540fe31f4f1c858c12
pubrrr <pubr@posteo.de>
2024-12-27 19:28:11 +0100

    feat: improve Telegram channel buttons

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,28 +1,19 @@
 import { ConcertList } from './ConcertList.tsx';
 import { StyledLink } from './StyledLink.tsx';
+import { GenreFilters, iconMap } from './types.ts';
+import { FC } from 'react';
 
 function App() {
     return (
         <div className='relative flex flex-col items-center text-center text-base-content'>
             <ScrollToTop />
-            <div className='m-8 flex flex-col items-center'>
+            <div className='m-8 flex flex-col items-center gap-2'>
                 <h1 className='mb-4 text-3xl font-bold text-primary'>All Concerts in Munich</h1>
 
-                <p>
-                    <a className='link' href='https://t.me/MunichMetalConcerts'>
-                        join the telegram METAL channel to get the newest updates
-                    </a>
-                </p>
-                <p>
-                    <a className='link' href='https://t.me/MunichRockConcerts'>
-                        join the telegram ROCK channel to get the newest updates
-                    </a>
-                </p>
-                <p>
-                    <a className='link mb-4' href='https://t.me/MunichPunkConcerts'>
-                        join the telegram PUNK channel to get the newest updates
-                    </a>
-                </p>
+                <p>Join the Telegram channels to get the newest updates:</p>
+                <TelegramButton genre='metal' href='https://t.me/MunichMetalConcerts' />
+                <TelegramButton genre='rock' href='https://t.me/MunichRockConcerts' />
+                <TelegramButton genre='punk' href='https://t.me/MunichPunkConcerts' />
             </div>
             <ConcertList />
             <p className='mb-16 text-xs'>
@@ -47,9 +38,23 @@ const ScrollToTop = () => (
             className='btn btn-circle btn-accent sticky bottom-4 z-[999]'
             aria-label='Scroll to top'
         >
-            <span className='iconify mdi--chevron-up text-2xl' />
+            <span className='iconify text-2xl mdi--chevron-up' />
         </a>
     </div>
 );
+
+type TelegramButtonProps = {
+    genre: keyof GenreFilters;
+    href: string;
+};
+
+const TelegramButton: FC<TelegramButtonProps> = ({ genre, href }) => {
+    return (
+        <a role='button' className='btn btn-neutral btn-sm' href={href}>
+            <span className={`iconify ${iconMap[genre]}`} />
+            <span className='capitalize'>{genre} Channel</span>
+        </a>
+    );
+};
 
 export default App;
